ef0a9ca40acdddcfc8199c925e05d031793589c1
mia17 <mia17@392cae34-d5b3-4f98-9cd9-ef8bba867bda>
2019-11-25 00:54:57 +0000

    Added feature that allows users to view their overall & individual car history

--- code/Car-Sharing/src/main/java/springData/controller/UserController.java
+++ code/Car-Sharing/src/main/java/springData/controller/UserController.java
@@ -1,7 +1,10 @@
 package springData.controller;
 
 import java.security.Principal;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Locale;
 
 import javax.validation.Valid;
 
@@ -20,6 +23,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import springData.DTO.RequestDTO;
+import springData.DTO.UserDTO;
 import springData.domain.Car;
 import springData.domain.CarAvailability;
 import springData.domain.Request;
@@ -48,7 +52,7 @@ public class UserController {
    @Autowired RequestRepository requestRepo;
 
    @InitBinder("requestDTO")
-   protected void initUserDTOBinder(WebDataBinder binder) {
+   protected void initRequestDTOBinder(WebDataBinder binder) {
       binder.addValidators(new RequestDTOValidator(requestRepo));
    }
 
@@ -56,16 +60,27 @@ public class UserController {
    public String createUser(@Valid @ModelAttribute("requestDTO") RequestDTO requestDTO, BindingResult result,
            Model model, Principal principal) {
 
+      
       if (result.hasErrors()) {
+         System.out.println("Lukas likes wings 55");
          //Add Car Request form
-         RequestDTO newRequestDTO = new RequestDTO();
-         model.addAttribute("requestDTO", newRequestDTO);
+         //RequestDTO newRequestDTO = new RequestDTO();
+         model.addAttribute("requestDTO", requestDTO);
+         
+         System.err.println(result);
          return "/user/dashboard";
-
       } else {
+         System.out.println("Lukas likes wings");
          //Create new User using UserDTO details
          Request newRequest = new Request();
 
+/*         String time = "05:32 AM";
+         //time = time.split(" "); 
+         String result = time.substring(0, time.length() - 3);
+         
+        System.out.println(LocalTime.parse(result));*/
+         //String result = LocalTime.parse("03:30 PM" , DateTimeFormatter.ofPattern("hh:mm a" , Locale.US)).format(DateTimeFormatter.ofPattern("HH:mm"));
+         
          newRequest.setStartTime(requestDTO.getStartTime());
          newRequest.setEndTime(requestDTO.getEndTime());
          newRequest.setRequestDate(requestDTO.getRequestDate());
@@ -75,23 +90,22 @@ public class UserController {
 
          //Find available Car
          newRequest.setCar(findCar(newRequest));
-
+         System.out.println("it is a dish");
          //Save Request
          requestRepo.save(newRequest);
 
-        // model.addAttribute("startTime", newRequest.getStartTime());
-        // model.addAttribute("endTime", newRequest.getEndTime());
-        // model.addAttribute("accessCode", newRequest.getCar().getCarAvailability().getAccessCode());
-        // model.addAttribute("carColor", newRequest.getCar().getCarColor());
-        // model.addAttribute("carName", newRequest.getCar().getCarName());
-        // model.addAttribute("registrationNumber", newRequest.getCar().getRegistrationNumber());
          model.addAttribute("requestID", newRequest.getRequestID());
 
-         return "redirect:/user/requestCar";
+         return "redirect:/user/requestCars";
       }
    }
+   
+   public LocalTime timeConverter(String time) {
+      String convert = LocalTime.parse(time, DateTimeFormatter.ofPattern("hh:mm a", Locale.US)).format(DateTimeFormatter.ofPattern("hh:mm"));
+      return LocalTime.parse(convert);
+   }
 
-   @GetMapping("/requestCar")
+   @GetMapping("/requestCars")
    public String viewUser(@RequestParam(value = "requestID", required = true) int requestID, Model model) {
       //Find Request by ID
       Request newRequest = requestRepo.findById(requestID);
@@ -123,19 +137,38 @@ public class UserController {
    @GetMapping("/view-request/{requestID}")
    public String viewRequest(@PathVariable int requestID, Model model) {
       //Find Request by ID
-      Request newRequest = requestRepo.findById(requestID);
+      Request request = requestRepo.findById(requestID);
 
       //Add details to model
-      model.addAttribute("startTime", newRequest.getStartTime());
-      model.addAttribute("endTime", newRequest.getEndTime());
-      model.addAttribute("accessCode", newRequest.getCar().getCarAvailability().getAccessCode());
-      model.addAttribute("carColor", newRequest.getCar().getCarColor());
-      model.addAttribute("carName", newRequest.getCar().getCarName());
-      model.addAttribute("registrationNumber", newRequest.getCar().getRegistrationNumber());
+      model.addAttribute("startTime", request.getStartTime());
+      model.addAttribute("endTime", request.getEndTime());
+      model.addAttribute("accessCode", request.getCar().getCarAvailability().getAccessCode());
+      model.addAttribute("carColor", request.getCar().getCarColor());
+      model.addAttribute("carName", request.getCar().getCarName());
+      model.addAttribute("registrationNumber", request.getCar().getRegistrationNumber());
 
       return "/user/view-request";
    }
+   
+   @GetMapping("/profile")
+   public String account(Model model, Principal principal) {
+      //Find User
+      User user = userRepo.findByUsername(principal.getName());
 
+      //Display User info using UserDTO
+      UserDTO userDTO = new UserDTO();
+
+      userDTO.setFirstName(user.getFirstName());
+      userDTO.setLastName(user.getLastName());
+      userDTO.setUsername(user.getUsername());
+      userDTO.setPhoneNumber(user.getPhoneNumber());
+      userDTO.setDriverLicense(user.getDriverLicense());
+
+      model.addAttribute("userId", user.getUserID());
+      model.addAttribute("userDTO", userDTO);
+
+      return "user/profile";
+   }
 
    //Function that finds 1st available Car
    private Car findCar(Request request) {
