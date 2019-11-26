83bc805b7209d32179e2707b6a8e7418bfeca915
Mohammed I. Ahmed <14203224+moa38@users.noreply.github.com>
2019-11-26 08:38:46 +0000

    Pre-prototype commit

--- src/main/java/springData/controller/UserDTOValidator.java
+++ src/main/java/springData/controller/UserDTOValidator.java
@@ -26,8 +26,8 @@ public class UserDTOValidator implements Validator {
       ValidationUtils.rejectIfEmptyOrWhitespace(errors, "firstName", "", "Enter a First Name");
       ValidationUtils.rejectIfEmptyOrWhitespace(errors, "lastName", "", "Enter a Last Name");
       ValidationUtils.rejectIfEmptyOrWhitespace(errors, "username", "", "Username is required");
-      ValidationUtils.rejectIfEmptyOrWhitespace(errors, "password", "", "Please enter a password");
-      ValidationUtils.rejectIfEmptyOrWhitespace(errors, "matchingPassword", "", "Please enter a matching password");
+      //ValidationUtils.rejectIfEmptyOrWhitespace(errors, "password", "", "Please enter a password");
+     // ValidationUtils.rejectIfEmptyOrWhitespace(errors, "matchingPassword", "", "Please enter a matching password");
 
       //Ensure First Name is valid
       if ((dto.getFirstName() != null) && (dto.getFirstName().length() > 0) && (dto.getFirstName().length() < 2)) {
@@ -38,14 +38,14 @@ public class UserDTOValidator implements Validator {
          errors.rejectValue("lastName", "", "Last name has less than 2 characters.");
       }
       //Ensure Username is not in use
-      if (userRepo.findByUsername(dto.getUsername()) != null) {
-         errors.rejectValue("username", "", "Username is unavailable.");
-      }
+      //if (userRepo.findByUsername(dto.getUsername()) != null) {
+      //   errors.rejectValue("username", "", "Username is unavailable.");
+    //  }
       //Ensure Username is valid
       if ((dto.getUsername() != null) && (dto.getUsername().length() > 0) && (dto.getUsername().length() < 4)) {
          errors.rejectValue("username", "", "Username has less than 4 characters.");
       }
-      //Ensure Password is entered
+      /*Ensure Password is entered
       if ((dto.getPassword() != null) && (dto.getPassword().length() > 0) && (dto.getPassword().length() < 4)) {
          errors.rejectValue("password", "", "Password must have more than 4 characters.");
          System.out.println("Password error");
@@ -60,7 +60,7 @@ public class UserDTOValidator implements Validator {
       if (!(dto.getPassword().equals(dto.getMatchingPassword()))) {
          errors.rejectValue("password", "", "Password must match.");
          errors.rejectValue("matchingPassword", "", "Passwords must match.");
-      }
+      }*/
    }
 
 }
