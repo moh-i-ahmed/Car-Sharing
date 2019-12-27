30a520c3319926d77d09824ab4e07103c8a360ee
Mohammed I. Ahmed <14203224+moa38@users.noreply.github.com>
2019-12-27 13:59:49 +0000

    Refactored domain classes

--- src/main/java/springData/domain/Car.java
+++ src/main/java/springData/domain/Car.java
@@ -3,13 +3,10 @@ package springData.domain;
 import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
-import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.Type;
 
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
