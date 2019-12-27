f77f8e00b0654242100c8f318f76e2badb70b116
mia17 <mia17@392cae34-d5b3-4f98-9cd9-ef8bba867bda>
2019-12-27 14:06:56 +0000

    Added check preventing user from making multiple requests

--- code/Car-Sharing/src/main/java/springData/controller/AuthenticationController.java
+++ code/Car-Sharing/src/main/java/springData/controller/AuthenticationController.java
@@ -8,17 +8,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import springData.DTO.RequestDTO;
+import springData.domain.Request;
 import springData.domain.User;
 import springData.repository.CarRepository;
+import springData.repository.RequestRepository;
 import springData.repository.UserRepository;
 
 import java.security.Principal;
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 @Controller
 public class AuthenticationController {
 
    @Autowired UserRepository userRepo;
    @Autowired CarRepository carRepo;
+   @Autowired RequestRepository requestRepo;
 
    @RequestMapping(path = "/", method = RequestMethod.GET)
    public String landing() {
@@ -34,17 +39,17 @@ public class AuthenticationController {
 
       //Get View for Role
       switch (user.getRole().getRole()) {
-         case "ADMIN":
-            view = "redirect:/dashboard";
-            break;
-         default:
-            view = "redirect:/dashboard";
-            break;
+      case "ADMIN":
+         view = "redirect:/dashboard";
+         break;
+      default:
+         view = "redirect:/dashboard";
+         break;
       }
       return view;
    }
 
-//   add cookies
+   //   add cookies
    @RequestMapping (value = "/dashboard", method = RequestMethod.GET)
    public String dashboard(Model model, Principal principal) {
       //Get Logged in User
@@ -58,15 +63,30 @@ public class AuthenticationController {
          model.addAttribute("userCount", userRepo.findAll().size());
          model.addAttribute("totalCars", carRepo.findAll().size());
          model.addAttribute("activeCars", carRepo.findAllAvailable().size());
-         
+         model.addAttribute("requests", requestRepo.findAll().size());
+
          return "/admin/dashboard";
       }
       //User view
       else {
-         //Add Car Request form
-         RequestDTO requestDTO = new RequestDTO();
-         model.addAttribute("requestDTO", requestDTO);
-         return "/user/dashboard";
+         //Check if car request is currently active
+         //if(user.isActive() == true) {
+         Request request = requestRepo.findLatestByDate();
+
+         if(!(request == null) && request.getRequestDate().isEqual(LocalDate.now()) &&
+               request.getEndTime().isAfter(LocalTime.now())) {
+            
+            System.err.println("Car already active");
+            model.addAttribute("requestID", request.getRequestID());
+
+            return "redirect:/user/request";
+         }
+         else {
+            //Add Car Request form
+            RequestDTO requestDTO = new RequestDTO();
+            model.addAttribute("requestDTO", requestDTO);
+            return "/user/dashboard";
+         }
       }
    }
 
