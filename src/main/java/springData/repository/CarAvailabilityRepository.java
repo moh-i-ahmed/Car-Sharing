cdedadda669a00fbac957b9602bc97384825eddb
Mohammed I. Ahmed <14203224+moa38@users.noreply.github.com>
2019-12-27 13:58:18 +0000

    Added JpaRepository queries

--- src/main/java/springData/repository/CarAvailabilityRepository.java
+++ src/main/java/springData/repository/CarAvailabilityRepository.java
@@ -1,10 +1,6 @@
 package springData.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import springData.domain.CarAvailability;
 
--- src/main/java/springData/repository/RequestRepository.java
+++ src/main/java/springData/repository/RequestRepository.java
@@ -15,6 +15,16 @@ public interface RequestRepository extends JpaRepository<Request, Integer> {
 
    @Query("Select e from Request e where e.user= :user")
    List<Request> findAllByUser(@Param("user") User user);
+   
+   /*
+   SELECT t1.*
+   FROM lms_attendance t1
+   WHERE t1.time = (SELECT MAX(t2.time)
+                    FROM lms_attendance t2
+                    WHERE t2.user = t1.user) */
+   
+   @Query("Select e from Request e where e.requestDate = (Select MAX(s.requestDate) from Request s where s.user = e.user)")
+   Request findLatestByDate();
 
    @SuppressWarnings("unchecked")
    Request save(Request request);
