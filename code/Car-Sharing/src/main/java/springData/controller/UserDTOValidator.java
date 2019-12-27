e127e32b9d42a1c3ff4652707539a7b6a2232cfa
mia17 <mia17@392cae34-d5b3-4f98-9cd9-ef8bba867bda>
2019-12-27 14:03:26 +0000

    Refactored DTO Validators

--- code/Car-Sharing/src/main/java/springData/controller/UserDTOValidator.java
+++ code/Car-Sharing/src/main/java/springData/controller/UserDTOValidator.java
@@ -26,8 +26,6 @@ public class UserDTOValidator implements Validator {
       ValidationUtils.rejectIfEmptyOrWhitespace(errors, "firstName", "", "Enter a First Name");
       ValidationUtils.rejectIfEmptyOrWhitespace(errors, "lastName", "", "Enter a Last Name");
       ValidationUtils.rejectIfEmptyOrWhitespace(errors, "username", "", "Username is required");
-      //ValidationUtils.rejectIfEmptyOrWhitespace(errors, "password", "", "Please enter a password");
-     // ValidationUtils.rejectIfEmptyOrWhitespace(errors, "matchingPassword", "", "Please enter a matching password");
 
       //Ensure First Name is valid
       if ((dto.getFirstName() != null) && (dto.getFirstName().length() > 0) && (dto.getFirstName().length() < 2)) {
@@ -37,15 +35,11 @@ public class UserDTOValidator implements Validator {
       if ((dto.getLastName() != null) && (dto.getLastName().length() > 0) && (dto.getLastName().length() < 2)) {
          errors.rejectValue("lastName", "", "Last name has less than 2 characters.");
       }
-      //Ensure Username is not in use
-      //if (userRepo.findByUsername(dto.getUsername()) != null) {
-      //   errors.rejectValue("username", "", "Username is unavailable.");
-    //  }
       //Ensure Username is valid
       if ((dto.getUsername() != null) && (dto.getUsername().length() > 0) && (dto.getUsername().length() < 4)) {
          errors.rejectValue("username", "", "Username has less than 4 characters.");
       }
-      /*Ensure Password is entered
+      //Ensure Password is entered
       if ((dto.getPassword() != null) && (dto.getPassword().length() > 0) && (dto.getPassword().length() < 4)) {
          errors.rejectValue("password", "", "Password must have more than 4 characters.");
          System.out.println("Password error");
@@ -57,10 +51,10 @@ public class UserDTOValidator implements Validator {
          System.out.println("Password error");
       }
       //Ensure Passwords match
-      if (!(dto.getPassword().equals(dto.getMatchingPassword()))) {
+      if ((dto.getPassword() != null) && (dto.getMatchingPassword() != null) && !(dto.getPassword().equals(dto.getMatchingPassword()))) {
          errors.rejectValue("password", "", "Password must match.");
-         errors.rejectValue("matchingPassword", "", "Passwords must match.");
-      }*/
+         errors.rejectValue("matchingPassword", "", "Password must match.");
+      }
    }
 
 }
