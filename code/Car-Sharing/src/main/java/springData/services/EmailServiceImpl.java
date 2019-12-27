164ff0a84d34796eb14aca2e459975896319414f
mia17 <mia17@392cae34-d5b3-4f98-9cd9-ef8bba867bda>
2019-12-27 13:59:11 +0000

    Added functions that process email requests

--- code/Car-Sharing/src/main/java/springData/services/EmailServiceImpl.java
+++ code/Car-Sharing/src/main/java/springData/services/EmailServiceImpl.java
@@ -1,31 +1,81 @@
 package springData.services;
 
-import javax.mail.internet.InternetAddress;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.stereotype.Service;
-
 import it.ozimov.springboot.mail.model.Email;
 import it.ozimov.springboot.mail.model.defaultimpl.DefaultEmail;
 import it.ozimov.springboot.mail.service.EmailService;
+import it.ozimov.springboot.mail.service.exception.CannotSendEmailException;
+import springData.DTO.UserDTO;
+import springData.constants.Constants;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.google.common.collect.ImmutableMap;
+
+import javax.mail.internet.InternetAddress;
+import java.io.UnsupportedEncodingException;
+import java.net.URISyntaxException;
+
+import java.util.Map;
+
+import static com.google.common.collect.Lists.newArrayList;
 
 @Service
 public class EmailServiceImpl {
-  
-   private JavaMailSender mailSender;
-   
+
    @Autowired
    public EmailService emailService;
 
-  /* public void sendEmailWithoutTemplating(){
-      final Email email = DefaultEmail.builder()
-           .from(new InternetAddress("cicero@mala-tempora.currunt", "Marco Tullio Cicerone "))
-           .to(Lists.newArrayList(new InternetAddress("titus@de-rerum.natura", "Pomponius Attĭcus")))
-           .subject("Laelius de amicitia")
-           .body("Firmamentum autem stabilitatis constantiaeque eius, quam in amicitia quaerimus, fides est.")
-           .encoding("UTF-8").build();
+   public void sendMimeEmailWithFreemarker(String subject, String template, UserDTO userDTO, Map<String, Object> modelObject)
+         throws UnsupportedEncodingException, CannotSendEmailException, URISyntaxException {
 
-      emailService.send(email);
-   }*/
+      final Email email = DefaultEmail.builder()
+            .from(new InternetAddress("web.carsharing@gmail.com", "Car Sharing"))
+            .to(newArrayList(new InternetAddress(userDTO.getUsername(), userDTO.getFirstName())))
+            .subject(subject)
+            .body("")//this will be overridden by the template, anyway
+            .encoding("UTF-8").build();
+
+      emailService.send(email, template, modelObject);
+   }
+
+   //Function processing registration email requests
+   public void sendRegistrationEmail(UserDTO userDTO) {
+      try {
+         String subject = "";
+         String template = "";
+         Map<String, Object> modelObject = null;
+
+         subject = Constants.REGISTRATIONSUBJECT;
+         template = Constants.REGISTRATIONTEMPLATE;
+         modelObject = ImmutableMap.of(
+               "name", userDTO.getFirstName()
+               );
+
+         //Send email with required info
+         sendMimeEmailWithFreemarker(subject, template, userDTO, modelObject);
+      } catch (UnsupportedEncodingException | CannotSendEmailException | URISyntaxException e) {
+         e.printStackTrace();
+      }
+   }
+
+   //Function processing password reset email requests
+   public void sendResetEmail(UserDTO userDTO) {
+      try {
+         Map<String, Object> modelObject = null;
+
+         modelObject = ImmutableMap.of(
+               "password", userDTO.getPassword(),
+               "name", userDTO.getFirstName()
+               );
+
+         //Send email with required info
+         sendMimeEmailWithFreemarker(Constants.PASSWORDSUBJECT, Constants.PASSWORDTEMPLATE,
+               userDTO, modelObject);
+      } catch (UnsupportedEncodingException | CannotSendEmailException | URISyntaxException e) {
+         e.printStackTrace();
+      }
+   }
+
 }
+//EmailServiceImpl
