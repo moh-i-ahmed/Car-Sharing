df624a13a908562270e973418f7dc7ff54cacd69
Mohammed I. Ahmed <14203224+moa38@users.noreply.github.com>
2019-12-27 13:56:40 +0000

    Added constants file

---
+++ src/main/java/springData/constants/Constants.java
@@ -0,0 +1,13 @@
+package springData.constants;
+
+public class Constants {
+
+   public static final String REGISTRATIONSUBJECT = "Registration: Thanks for Joining";
+   public static final String REGISTRATIONTEMPLATE = "registration.ftl";
+
+   public static final String PASSWORDSUBJECT = "Password Reset";
+   public static final String PASSWORDTEMPLATE = "passwordReset.ftl";
+
+}
+//Constants
+
