a7cef1607572bf6669f753b8943973b762c182fe
mia17 <mia17@392cae34-d5b3-4f98-9cd9-ef8bba867bda>
2019-12-27 14:00:58 +0000

    Updated project config files

--- code/Car-Sharing/src/main/java/springData/WebConfig.java
+++ code/Car-Sharing/src/main/java/springData/WebConfig.java
@@ -1,6 +1,9 @@
 package springData;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.ui.freemarker.FreeMarkerConfigurationFactoryBean;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -39,6 +42,14 @@ public class WebConfig implements WebMvcConfigurer {
               "classpath:/static/css/",
               "classpath:/static/vendor/",
               "classpath:/static/js/");
+   }
+   
+   @Primary
+   @Bean 
+   public FreeMarkerConfigurationFactoryBean factoryBean() {
+      FreeMarkerConfigurationFactoryBean bean=new FreeMarkerConfigurationFactoryBean();
+      bean.setTemplateLoaderPath("classpath:/templates/email/");
+      return bean;
    }
 
 }
