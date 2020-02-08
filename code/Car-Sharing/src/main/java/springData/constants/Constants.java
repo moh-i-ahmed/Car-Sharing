540e6d464ccd316a923144384537b8bf9df8308b
mia17 <mia17@392cae34-d5b3-4f98-9cd9-ef8bba867bda>
2020-02-08 01:05:24 +0000

    Added request status to Constants

--- code/Car-Sharing/src/main/java/springData/constants/Constants.java
+++ code/Car-Sharing/src/main/java/springData/constants/Constants.java
@@ -8,6 +8,10 @@ public class Constants {
    public static final String PASSWORDSUBJECT = "Password Reset";
    public static final String PASSWORDTEMPLATE = "passwordReset.ftl";
 
+   public static final String STATUS_COMPLETE = "Completed";
+   public static final String STATUS_IN_PROGRESS = "In Progress";
+   public static final String STATUS_CANCELLED = "Cancelled";
+
 }
 //Constants
 
