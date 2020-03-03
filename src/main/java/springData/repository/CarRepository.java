62fcd3877235ea6d425ed7988cb70b51ef000322
Mohammed I. Ahmed <14203224+moa38@users.noreply.github.com>
2020-03-03 22:59:10 +0000

    Added new query to CarRepository

--- src/main/java/springData/repository/CarRepository.java
+++ src/main/java/springData/repository/CarRepository.java
@@ -4,12 +4,14 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import springData.domain.Car;
 
 public interface CarRepository extends JpaRepository<Car, Integer> {
 
-   Car findById(int id);
+   @Query("Select u from Car u where u.registrationNumber = :registrationNumber")
+   Car findByRegistrationNumber(@Param("registrationNumber") String registrationNumber);
 
    @Query("Select e from Car e where e.isActive = FALSE")
    List<Car> findAllAvailable();
