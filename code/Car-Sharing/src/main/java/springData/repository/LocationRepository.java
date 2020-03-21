611aa064eff71e46d616b95b543fa2e976b2cf2d
mia17 <mia17@392cae34-d5b3-4f98-9cd9-ef8bba867bda>
2020-03-21 14:51:49 +0000

    Added repository for Location entity

---
+++ code/Car-Sharing/src/main/java/springData/repository/LocationRepository.java
@@ -0,0 +1,20 @@
+package springData.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import springData.domain.Location;
+
+public interface LocationRepository extends JpaRepository<Location, Integer> {
+
+   Location findById(int id);
+
+   @Query("Select l from Location l where l.latitude = :latitude")
+   Location findByLatitude(@Param("latitude") String latitude);
+
+   @SuppressWarnings("unchecked")
+   Location save(Location location);
+}
