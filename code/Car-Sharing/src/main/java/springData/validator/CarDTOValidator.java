d33a761261a68da025a6b6b7b18bce637118107b
mia17 <mia17@392cae34-d5b3-4f98-9cd9-ef8bba867bda>
2020-03-21 14:49:53 +0000

    Updated validators for carDTO

--- code/Car-Sharing/src/main/java/springData/validator/CarDTOValidator.java
+++ code/Car-Sharing/src/main/java/springData/validator/CarDTOValidator.java
@@ -5,8 +5,6 @@ import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 
 import springData.DTO.CarDTO;
-import springData.DTO.UserDTO;
-import springData.repository.CarRepository;
 
 public class CarDTOValidator implements Validator {
 
@@ -23,18 +21,40 @@ public class CarDTOValidator implements Validator {
 
       ValidationUtils.rejectIfEmptyOrWhitespace(errors, "registrationNumber", "", "Enter a Registration Number");
       ValidationUtils.rejectIfEmptyOrWhitespace(errors, "carName", "", "Enter a Brand/Name");
-      ValidationUtils.rejectIfEmptyOrWhitespace(errors, "carMake", "", "Enter a model/make");
+      ValidationUtils.rejectIfEmptyOrWhitespace(errors, "carMake", "", "Enter a Model/Make");
+      ValidationUtils.rejectIfEmptyOrWhitespace(errors, "carColor", "", "Enter a color");
+      ValidationUtils.rejectIfEmptyOrWhitespace(errors, "isActive", "", "Select a status");
+      ValidationUtils.rejectIfEmptyOrWhitespace(errors, "fuelLevel", "", "Enter a fuel level");
 
-      //Ensure Registration Number is within minimum length
+      //Ensure Registration Number is valid
       if ((dto.getRegistrationNumber() != null) && (dto.getRegistrationNumber().length() > 0) && (dto.getRegistrationNumber().length() < 6)) {
-         errors.rejectValue("registrationNumber", "", "First name has less than 2 characters.");
+         errors.rejectValue("registrationNumber", "", "Registration number has less than 2 characters.");
+      }
+      //Ensure Registration Number is valid
+      if ((dto.getRegistrationNumber() != null) && (dto.getRegistrationNumber().length() > 0) && (dto.getRegistrationNumber().length() > 10)) {
+         errors.rejectValue("registrationNumber", "", "Registration number is too long.");
       }
       //Ensure carName is within maximum length
       if ((dto.getCarName() != null) && (dto.getCarName().length() > 0) && (dto.getCarName().length() > 255)) {
-         errors.rejectValue("carName", "", "First name is too long. Maximum length 255.");
+         errors.rejectValue("carName", "", "Brand name is too long. Maximum length 255.");
+      }
+      //Ensure carMake is within maximum length
+      if ((dto.getCarMake() != null) && (dto.getCarMake().length() > 0) && (dto.getCarMake().length() > 255)) {
+         errors.rejectValue("carMake", "", "Make name is too long. Maximum length 255.");
+      }
+      //Ensure carColor is within maximum length
+      if ((dto.getCarColor() != null) && (dto.getCarColor().length() > 0) && (dto.getCarColor().length() > 255)) {
+         errors.rejectValue("carColor", "", "Color is too long. Maximum length 255.");
+      }
+      //Ensure isActive is valid
+      if ((dto.getIsActive()) && (dto.getCarColor().length() > 0) && (dto.getCarColor().length() > 255)) {
+         errors.rejectValue("isActive", "", "Color name is too long. Maximum length 255.");
+      }
+      //Ensure fuelLevel is within 0 to 100
+      if ((dto.getFuelLevel() < 0) || (dto.getFuelLevel() > 100)) {
+         errors.rejectValue("fuelLevel", "", "Fuel level must be between 0 and 100.");
       }
 
    }
-
 }
 //CarDTOValidator
