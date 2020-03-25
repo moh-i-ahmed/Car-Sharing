72769be1c7d5bd996ce44d045b321d4e7300a9c2
mia17 <mia17@392cae34-d5b3-4f98-9cd9-ef8bba867bda>
2020-03-25 23:54:45 +0000

    Added repository for StripeCustomer entity

---
+++ code/Car-Sharing/src/main/java/springData/repository/StripeCustomerRepository.java
@@ -0,0 +1,14 @@
+package springData.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import springData.domain.StripeCustomer;
+
+public interface StripeCustomerRepository extends JpaRepository<StripeCustomer, Integer> {
+
+   @Query("Select c from StripeCustomer c where c.tokenID = :tokenID")
+   StripeCustomer findByTokenID(@Param("tokenID") String tokenID);
+
+}
