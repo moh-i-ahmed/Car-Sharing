5cf483ae1ce4eba65b1ee5464735f413191fc933
Mohammed I. Ahmed <14203224+moa38@users.noreply.github.com>
2020-04-13 13:43:50 +0000

    Added verification tokens for account registration feature

--- src/main/java/springData/controller/RegistrationController.java
+++ src/main/java/springData/controller/RegistrationController.java
@@ -1,5 +1,8 @@
 package springData.controller;
 
+import java.time.LocalDateTime;
+
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,16 +14,19 @@ import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import springData.DTO.UserDTO;
+import springData.constants.Constants;
 import springData.domain.User;
+import springData.domain.VerificationToken;
 import springData.repository.RoleRepository;
 import springData.repository.UserRepository;
+import springData.repository.VerificationTokenRepository;
 import springData.services.EmailServiceImpl;
 import springData.utils.PasswordGenerator;
 import springData.validator.PasswordDTOValidator;
@@ -30,12 +36,14 @@ import springData.validator.UserDTOValidator;
 @RequestMapping("/register")
 public class RegistrationController {
 
-   Logger logger = LoggerFactory.getLogger(RegistrationController.class);
+   private static final Logger logger = LoggerFactory.getLogger(RegistrationController.class);
 
    BCryptPasswordEncoder pe = new  BCryptPasswordEncoder();
 
-   @Autowired UserRepository userRepo;
-   @Autowired RoleRepository roleRepo;
+   @Autowired private UserRepository userRepo;
+   @Autowired private RoleRepository roleRepo;
+   @Autowired private VerificationTokenRepository verificationTokenRepo;
+
    @Autowired private EmailServiceImpl emailService;
 
    @InitBinder("userDTO")
@@ -58,7 +66,8 @@ public class RegistrationController {
    }
 
    @PostMapping("/create")
-   public String createUser(@Valid @ModelAttribute("userDTO") UserDTO userDTO, BindingResult result, Model model) {
+   public String createUser(@Valid @ModelAttribute("userDTO") UserDTO userDTO, BindingResult result,
+         HttpServletRequest request, Model model) {
 
       //Check if username is already in use
       User userExists = userRepo.findByUsername(userDTO.getUsername());
@@ -79,18 +88,52 @@ public class RegistrationController {
          newUser.setFirstName(userDTO.getFirstName());
          newUser.setLastName(userDTO.getLastName());
          newUser.setUsername(userDTO.getUsername());
+         newUser.setDriverLicense(userDTO.getDriverLicense());
          newUser.setPassword(pe.encode(userDTO.getPassword()));
+         newUser.setEnabled(false);
 
          newUser.setRole(roleRepo.findByRoleName("USER"));
 
          //Save User
          userRepo.save(newUser);
+         try {
+            VerificationToken token = new VerificationToken(newUser);
+            verificationTokenRepo.save(token);
 
-         //Send Email
-         emailService.sendRegistrationEmail(userDTO);
-
+            String appUrl = request.getScheme() + Constants.SERVER_URL + "register/confirm/" + token.getToken();
+            System.err.println("App url: " + appUrl);
+            //Send Email
+            emailService.sendRegistrationEmail(userDTO, appUrl);
+         } catch (Exception e) {
+            logger.info("Error: " + e);
+         }
          logger.info("\n User registered: " + userDTO.getUsername()
          +"\n Registration email sent.");
+
+         return "redirect:/";
+      }
+   }
+
+   @GetMapping("/confirm/{token}")
+   public String confirm(@PathVariable String token, Model model) {
+      // Retrieve token
+      VerificationToken verificationToken = verificationTokenRepo.findByToken(token);
+
+      // Token invalid
+      if (verificationToken.getExpiryDate().isBefore(LocalDateTime.now())) {
+         // Notification message
+         String message = "Error: Link has expired";
+         model.addAttribute("message", message);
+
+         return "forgot-password";
+      }
+      else {
+         // Enable User Account
+         User user = verificationToken.getUser();
+         user.setEnabled(true);
+
+         // Save changes
+         userRepo.save(user);
 
          return "redirect:/";
       }
@@ -125,7 +168,7 @@ public class RegistrationController {
          emailService.sendResetEmail(userDTO);
 
          logger.info("\n Password reset for: " + userDTO.getUsername()
-               + "\n Reset email sent.");
+         + "\n Reset email sent.");
 
          // Save User
          user.setPassword(pe.encode(generatedPassword));   
---
+++ src/main/java/springData/repository/VerificationTokenRepository.java
@@ -0,0 +1,17 @@
+package springData.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import springData.domain.User;
+import springData.domain.VerificationToken;
+
+public interface VerificationTokenRepository extends JpaRepository<VerificationToken, Integer> {
+
+   @Query("Select t from VerificationToken t where t.user = :user")
+   VerificationToken findByUser(@Param("user") User user);
+
+   @Query("Select t from VerificationToken t where t.token= :token")
+   VerificationToken findByToken(@Param("token") String token);
+}
