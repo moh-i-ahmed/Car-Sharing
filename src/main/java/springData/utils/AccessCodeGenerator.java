3a84658ba4d79b599395954445eef7fdf3e8451a
Mohammed I. Ahmed <14203224+moa38@users.noreply.github.com>
2020-04-13 13:53:52 +0000

    Fixed bug in AccessCodeGenerator that caused heap error

--- src/main/java/springData/utils/AccessCodeGenerator.java
+++ src/main/java/springData/utils/AccessCodeGenerator.java
@@ -4,9 +4,12 @@ public class AccessCodeGenerator {
 
    private static final String ALPHA_NUMERIC_STRING = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
    
-   public static String generateAccessCode(int count) {
+   public static String generateAccessCode() {
+      // Code size
+      int size = 6;
+
       StringBuilder builder = new StringBuilder();
-      while (count-- != 0) {
+      while (size-- != 0) {
          int character = (int)(Math.random()*ALPHA_NUMERIC_STRING.length());
          builder.append(ALPHA_NUMERIC_STRING.charAt(character));
       }
